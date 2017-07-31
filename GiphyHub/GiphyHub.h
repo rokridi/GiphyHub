328783048975bec4e92fa1e145a1a10ca199f1b2
Mohamed Aymen Landolsi <mohamed.aymen.landolsi@gmail.com>
2017-07-31 12:21:38 +0200

    initial commit

---
+++ GiphyHub/GiphyHub.h
@@ -0,0 +1,19 @@
+//
+//  GiphyHub.h
+//  GiphyHub
+//
+//  Created by Mohamed Aymen Landolsi on 31/07/2017.
+//  Copyright © 2017 Roridi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GiphyHub.
+FOUNDATION_EXPORT double GiphyHubVersionNumber;
+
+//! Project version string for GiphyHub.
+FOUNDATION_EXPORT const unsigned char GiphyHubVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GiphyHub/PublicHeader.h>
+
+
